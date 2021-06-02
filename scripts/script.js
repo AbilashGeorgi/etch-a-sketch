5ba63b4f541cf0f29f8c690eb6bf77a74efc35e5
Abilash Georgi <abilashchacko@gmail.com>
2021-06-02 22:39:28 +0530

    Add Reset Button Dynamically add elements to the grid.

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,35 @@
-function initialise() {
+let gridColumns = 10;
+
+function initialiseGridElements(doResetOnly=false) {
     const gridElements = document.querySelectorAll('.grid div');
-    gridElements.forEach((element)=> {
-        element.addEventListener('mouseenter',(e) => {
-            e.target.classList.add('chosen');
-        }, {once:true})
-    });
+    if (doResetOnly) {
+        gridElements.forEach((element)=>{
+            element.classList.remove('chosen');
+        });
+    } else {
+        gridElements.forEach((element) => {
+            element.addEventListener('mouseenter', (e) => {
+                e.target.classList.add('chosen');
+            })
+            //,{once:true} for once executing listener
+        });
+    }
+}
+
+function initialiseGrid() {
+    const grid = document.querySelector('.grid');
+    grid.innerHTML = '';
+    grid.style.gridTemplateColumns = `repeat(${gridColumns}, auto)`;
+    for (let i=1; i<=gridColumns*gridColumns; i++) {
+        grid.innerHTML += '<div></div>';
+    }
+    initialiseGridElements();
+}
+
+function initialise() {
+    initialiseGrid()
+    const reset = document.querySelector('button#reset');
+    reset.addEventListener('click', () => {setTimeout(()=>{initialiseGridElements(true)}, 0)});
 }
 
 initialise();
